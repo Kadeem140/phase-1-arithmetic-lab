0491cdcff6bae162559fcb48ebe54f406a49606c
Kadeem140 <Beckford.kadeem@yahoo.com>
2022-02-19 07:58:17 -0500

    MVP

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 
 // Write your code here
+
+
+//1 
+
+const num1 = 31
+const num2 = 2
+function multiply(num1, num2){
+    return num1 * num2
+}
+
+//2
+const random = Math.random()
+
+//3
+function mod(num3, num4){
+    num3 = 64
+    num4 = 6
+    return num3 % num4
+}
+
+//4
+function max(){
+    num5= 5 
+    num6= 10
+    num7= 20
+    return Math.ceil(num5, num6, num7)
+    
+}
+
